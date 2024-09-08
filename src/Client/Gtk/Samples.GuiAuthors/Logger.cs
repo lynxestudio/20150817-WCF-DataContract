de5b85a6d35135e7e0ba234972ab72b0044f2306
Martin A. Ortega <lynxestudio@outlook.com>
2024-09-08 15:19:33 -0600

    Fixes the folders of the source code

---
+++ src/Client/Gtk/Samples.GuiAuthors/Logger.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using System;
+
+namespace Samples.GuiAuthors
+{
+	public static class Logger
+	{
+		public static void LogWriteError(string s){
+			using(FileStream stream = new FileStream("log.txt",FileMode.OpenOrCreate,FileAccess.ReadWrite)){
+				StreamWriter sw = new StreamWriter(stream);
+				sw.BaseStream.Seek(0,SeekOrigin.End);
+				sw.Write(DateTime.Now.ToString() + ": "+ s);
+				sw.Flush();
+				sw.Close();
+			}
+		}
+	}
+}
+
