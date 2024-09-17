51534c02bd09ac0c10bdc1ed16d8576688aa8091
Martin A. Ortega <lynxestudio@outlook.com>
2024-09-16 19:06:47 -0600

    Adds WinForm client source code

---
+++ src/Client/WinForms/Samples.AuthorsServiceClient/FrmMain.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Samples.AuthorsServiceClient
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.calBirthDate = new System.Windows.Forms.MonthCalendar();
+            this.chkGender = new System.Windows.Forms.CheckBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.authorsListStore = new System.Windows.Forms.DataGridView();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.authorsListStore)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(101, 20);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(177, 22);
+            this.txtFirstName.TabIndex = 2;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(101, 48);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(177, 22);
+            this.txtLastName.TabIndex = 3;
+            // 
+            // calBirthDate
+            // 
+            this.calBirthDate.Location = new System.Drawing.Point(290, 9);
+            this.calBirthDate.Name = "calBirthDate";
+            this.calBirthDate.TabIndex = 4;
+            // 
+            // chkGender
+            // 
+            this.chkGender.AutoSize = true;
+            this.chkGender.Location = new System.Drawing.Point(16, 92);
+            this.chkGender.Name = "chkGender";
+            this.chkGender.Size = new System.Drawing.Size(72, 20);
+            this.chkGender.TabIndex = 5;
+            this.chkGender.Text = "Gender";
+            this.chkGender.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(16, 171);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAddClick);
+            // 
+            // authorsListStore
+            // 
+            this.authorsListStore.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.authorsListStore.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.authorsListStore.Location = new System.Drawing.Point(0, 201);
+            this.authorsListStore.MultiSelect = false;
+            this.authorsListStore.Name = "authorsListStore";
+            this.authorsListStore.ReadOnly = true;
+            this.authorsListStore.Size = new System.Drawing.Size(603, 150);
+            this.authorsListStore.TabIndex = 7;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(152, 168);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(114, 26);
+            this.btnRefresh.TabIndex = 8;
+            this.btnRefresh.Text = "Refresh grid";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.BtnRefreshClick);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 351);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.authorsListStore);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.chkGender);
+            this.Controls.Add(this.calBirthDate);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.Text = "Authors Service - Client";
+            ((System.ComponentModel.ISupportInitialize)(this.authorsListStore)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.MonthCalendar calBirthDate;
+        private System.Windows.Forms.CheckBox chkGender;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView authorsListStore;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
+
---
+++ src/Client/WinForms/Samples.AuthorsServiceClient/FrmMain.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Samples.AuthorsService;
+using System.ServiceModel;
+
+namespace Samples.AuthorsServiceClient
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddClick(object sender, EventArgs e)
+        {
+            ClearForm();
+            try
+            {
+                var proxy = Getproxy();
+                Author a = new Author
+                {
+                    FirstName = txtFirstName.Text,
+                    LastName = txtLastName.Text,
+                    BirthDate = calBirthDate.SelectionRange.Start,
+                    Gender = chkGender.Checked
+                };
+                MessageBox.Show(proxy.CreateAuthor(a));
+                RefreshGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("See the log.");
+                Logger.LogWriteError(ex.Message);
+            }
+        }
+
+        private void BtnRefreshClick(object sender, EventArgs e)
+        {
+            try
+            {
+                RefreshGrid();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("See the log.");
+                Logger.LogWriteError(ex.Message);
+            }
+        }
+
+        void RefreshGrid()
+        {
+            var proxy = Getproxy();
+            List<Author> authors = proxy.GetAuthors();
+            authorsListStore.DataSource = authors;
+        }
+
+        void ClearForm()
+        {
+            txtFirstName.Text = txtLastName.Text = String.Empty;
+            chkGender.Checked = false;
+        }
+
+        IAuthorServiceContract Getproxy()
+        {
+            EndpointAddress address = new EndpointAddress("http://localhost:9000/");
+            BasicHttpBinding binding = new BasicHttpBinding();
+            IAuthorServiceContract proxy = ChannelFactory<IAuthorServiceContract>.CreateChannel(binding, address);
+            return proxy;
+        }
+    }
+}
